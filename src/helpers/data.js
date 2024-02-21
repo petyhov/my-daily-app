cc56628b96431924b96e14742dc6dbf1396df1b9
DenysPetukhov <Denys.Petukhov@onseo.biz>
2024-02-21 14:53:28 +0200

    fix fetch protocol

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -2,7 +2,8 @@ import { headers } from 'next/headers';
 import axios from 'axios';
 
 export const makeFetch = async (url) => {
+  const protocol = process.env.LOCAL_BUILD ? 'http' : 'https';
   const host = headers().get('host');
-  const data = await axios(`http://${host}${url}`);
+  const data = await axios(`${protocol}://${host}${url}`);
   return data.data;
 };
