c4cd9091d67cd8708770910abdbffb4b7dec520b
petyhov <48859592+petyhov@users.noreply.github.com>
2024-02-22 17:56:09 +0200

    fix env value (#8)

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -3,7 +3,8 @@ import { headers } from 'next/headers';
 import axios from 'axios';
 
 export const makeFetch = async (url) => {
-  const protocol = process.env.LOCAL_BUILD ? 'http' : 'https';
+  const localBuild = process.env.LOCAL_BUILD === 'true';
+  const protocol = localBuild ? 'http' : 'https';
   const host = headers().get('host');
   const data = await axios(`${protocol}://${host}${url}`);
   return data.data;
